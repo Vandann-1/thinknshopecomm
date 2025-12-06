6de73da7691b3325bc4567cecc0165f3e2effebe
Vandann-1 <vandanparmar6041@gmail.com>
2025-12-06 20:55:35 +0530

    databases

--- sketezo/settings.py
+++ sketezo/settings.py
@@ -115,13 +115,29 @@ WSGI_APPLICATION = 'sketezo.wsgi.application'
 
 
 
-# For Development Only
+# # For Development Only
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# Production Only (Render PostgreSQL)
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default="postgresql://thinknshop_user:liUyBbhCftZY5vme2It1qQn8rFgfPpdE@dpg-d4q4gau3jp1c739becl0-a.oregon-postgres.render.com/thinknshop",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
